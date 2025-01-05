63a2e35c59778c7fdad70f53a3dbf901d23395fd
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-01-05 11:20:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function shortcut(s1, s2) {
   // your code here
-    if (str1 && str2) {
-        return str1[0] + str2[0];
-    }
-    return '';
+	let result = "";
+  // Check if s1 is not empty, then add the first character
+  if (s1.length > 0) {
+    result += s1[0];
+  }
+
+  // Check if s2 is not empty, then add the first character
+  if (s2.length > 0) {
+    result += s2[0];
+  }
+
+  // Return the result
+  return result;
 }
 
 // Do not change the code below.
