fdc5642aa37db279d44c82429981793e23f7aa9e
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-01-05 11:10:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function shortcut(s1, s2) {
   // your code here
+  if (!str1 || !str2) {
+    return '';
+  }
+
+  // Extract the first letters and return them concatenated
+  return str1[0] + str2[0];
 }
 
 // Do not change the code below.
